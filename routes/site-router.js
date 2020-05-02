da6a67b6e55605a6a27b912c3d748fceb3a74d36
Maxime Lagrange <com.maximelagrange@gmail.com>
2020-05-02 16:10:47 +0200

    Created all the routes for the app

--- routes/site-router.js
+++ routes/site-router.js
@@ -1,17 +1,42 @@
-const express = require('express');
+const express = require("express");
 const siteRouter = express.Router();
-const isLoggedIn = require('./../middleware/isLoggedIn');
+const isLoggedIn = require("./../middleware/isLoggedIn");
+
+// GET
+siteRouter.get("/all-beers", isLoggedIn, (req, res, next) => {
+  res.render("all-beers");
+});
+
+// GET
+siteRouter.get("/beer-description", isLoggedIn, (req, res, next) => {
+  res.render("beer-description");
+});
+
+// GET
+siteRouter.get("/add-beer", isLoggedIn, (req, res, next) => {
+  res.render("add-beer");
+});
+
+siteRouter.get("/add-review", isLoggedIn, (req, res, next) => {
+    res.render("add-review");
+  });
+
+// GET
+siteRouter.get("/favorite-beers", isLoggedIn, (req, res, next) => {
+    res.render("favorite-beers");
+  });
+
+  
+
+  // GET
+  siteRouter.get("/profile/profile-page", isLoggedIn, (req, res, next) => {
+    res.render("profile/profile-page");
+  });
 
 
-// GET              
-siteRouter.get('/all-beers', isLoggedIn, (req, res, next) => {
-  res.render('all-beers');
-})
-
-// GET               
-siteRouter.get('/private', isLoggedIn, (req, res, next) => {
-  res.render('private');
-})
+// GET
+siteRouter.get("/private", isLoggedIn, (req, res, next) => {
+  res.render("private");
+});
 
 module.exports = siteRouter;
-
