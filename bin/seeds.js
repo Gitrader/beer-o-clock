37c19dc61ef0721d68bc0fae71434dcb77e6f6e6
Siiri Sikkilä <sisikkil@student.oulu.fi>
2020-05-04 14:37:43 +0300

    seed database working

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const Beer = require("./../models/beer-model");
+require("dotenv").config() // require env variables
 //probab have to require user model and review as well
 // remember to install punkapi on npm!
 
-const DB_NAME = "BeerOclock"; // this shouldn't be needed but for now seeds.js isn't run anywhere else
+// this shouldn't be needed but for now seeds.js isn't run anywhere else
 
 
 //using punkAPI to get a starter base for our db:
@@ -40,41 +41,87 @@ function getPunkBeers() { // will push punkBeers into our own array of beers
         });
         console.log("ourbeers punkbeer foraechin jälkeen:", ourBeers)
     })
-    .catch(error => console.log("error seeding from punkAPI", error))
+        .catch(error => console.log("error seeding from punkAPI", error))
 
     return ourBeers
 }
 // ... ENDING HERE IS NOT USED ATM BUT DONT REMOVE YET
 
-const beerList = []
-function getPunks () {
-    punkAPI.getBeers().then(gotbeers => {
-        console.log("eka alkio gotbeersistä ", gotbeers[0])
-        gotbeers.forEach(element => {
-            console.log(element.name)
-            beerList.push(element)
+function getBeerArray() {
+    // const emptyArr
+    let punkPromise = punkAPI.getBeers().then(gotbeers => {
+        const beerList = []
+        //console.log("eka alkio gotbeersistä ", gotbeers[0])
+        gotbeers.forEach(beerObj => {
+            // create a new beer based on our own model and take punkAPI beer properties to it
+            let newBeer = new Beer() // remember parenthesis!
+            newBeer.name = beerObj.name
+            newBeer.brewery = "BrewDog"
+            newBeer.image_url = beerObj.image_url
+            newBeer.alcoholVol = beerObj.abv
+
+            beerObj.ingredients.malt.forEach(malt => {
+                newBeer.malt.push(malt.name)
+            });
+            beerObj.ingredients.hops.forEach(hops => {
+                // could add here a condition that don't push if the name is already there!!!
+                newBeer.hops.push(hops.name)
+            });
+
+            newBeer.EBU = beerObj.ibu // look it's actually IBU in punkbeers!
+            beerList.push(newBeer)
         });
+        //console.log("1st item inside our beeerlist: ",newBeer[0])
         console.log("we get beers as the printout above states")
         //console.log("emptylist from inside getbeers.then block",emptyList)
-        return gotbeers
+        console.log(beerList)
+        return beerList // return beerList!
     })
+        .catch(err => {
+            console.log(err)
+        })
+    return punkPromise
 }
 
+
+function seedDatabase(beerArr) {
+    const beerPromises = beerArr.map(beerObj => {
+        return Beer.create({beerObj})
+    });
+    return beerPromises
+}
 
 
 // 1. CONNECT TO MONGOOSE
 mongoose.connect(
-    `mongodb://localhost:27017/${DB_NAME}`,
+    `mongodb://localhost:27017/BeerOclock`,
     {useNewUrlParser: true, useUnifiedTopology: true}
-    )
+)
     .then((x) => {
         console.log(`we are Connected to DB from seeds: ${x.connections[0].name}`);
-        const punkPR = getPunks()
-        createDbPr = Beer.create(beerList);
+        // WARNING!! DROPPING DB TO AVOID DUPLICATES!!
+        return x.connection.dropDatabase()
+    }).then((y) => {
+        const beerList = getBeerArray() // we are saving an array to variable beerList, right?
+        console.log("list of created beers, beerList", beerList)
+        return beerList // here we return a (pending) promise of getting a beer array
+    })
+    .then(beerList => { // if our promise was succesfully resolved, we get a beer array!
+        const createDbPr = Beer.create(beerList); // creating is a promise ({} adding all?)
         return createDbPr;
+
+    })
+    .then(DBcreated => {
+        console.log("created db of length: ", DBcreated.length)
     })
     .catch((err) => console.log(err));
 
+    //in case we need these:
+// // Drop the collection
+// db.collection_name.drop()
+
+// // Drop the database
+// db.dropDatabase()
 
 
 //console.log("synkronisesti", ourBeers)
