6774330990a1b68378b62f355c35ccf9fc7206a6
Siiri Sikkilä <sisikkil@student.oulu.fi>
2020-05-06 11:43:06 +0300

    add missing number in review model

--- models/review-model.js
+++ models/review-model.js
@@ -5,7 +5,7 @@ const reviewSchema = new Schema({
   beerId: {type: mongoose.ObjectId, ref: "Beer", required: true},
   userId: {type: mongoose.ObjectId, ref: "User", required: true},
   review: {type: String},
-  rating: {type: Number, enum: [0, 1, 2, 3, 5], required: true},
+  rating: {type: Number, enum: [0, 1, 2, 3, 4, 5], required: true},
 });
 
 const Review = mongoose.model("Review", reviewSchema);
